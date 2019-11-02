aa354677475198e601803be911c7d76726cecb35
Jack Fox <0xdeadbeef1@gmail.com>
2019-11-02 01:11:10 -0400

    Added test script

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
 local mainScript = io.open("test.lua", "r")
+local testScript = [===[
+	local te, st = require("test.lua")
+	memes = require("lol.lua")
+
+]===]
+
 
 local function findImports(script)
 	local imports = {}
-	for match in script:gmatch("l?o?c?a?l?%s*.-%s*=?%s*require%s*%(?%s*['\"][%w_/\\%.]+['\"]%s*%)?") do
+	for match in script:gmatch("l?o?c?a?l?%s*.-%s*=?%s*require%s*%(?%s*['\"][%w_/\\%.]+['\"]%s*%)?\n") do
 		local import = {}
 		import.IsLocal = match:match("local%s*") ~= nil
 		import.Name = match:match("l?o?c?a?l?%s*([%w_]+)%s*=") or #imports + 1
