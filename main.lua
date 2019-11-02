7bdd7f8f2fe2b4b68ba7c3dafaf5989b6f986d37
Peyton <peyton2465@outlook.com>
2019-11-02 00:06:45 -0500

    init

---
+++ main.lua
@@ -0,0 +1,25 @@
+local mainScript = io.open("test.lua", "r")
+
+local function findImports(script)
+	local imports = {}
+	for match in script:gmatch("l?o?c?a?l?%s*.-%s*=?%s*require%s*%(?%s*['\"][%w_/\\%.]+['\"]%s*%)?") do
+		local import = {}
+		import.IsLocal = match:match("local%s*") ~= nil
+		import.Name = match:match("l?o?c?a?l?%s*([%w_]+)%s*=") or #imports + 1
+		import.SourcePath = match:match("require%s*%(?%s*['\"]([%w_/\\%.]+)['\"]%s*%)?")
+		table.insert(imports, import)
+	end
+	return imports
+end
+
+local imports = findImports(mainScript:read("*all"))
+mainScript:close()
+
+for i,v in next, imports do
+	for l,k in next, v do
+		print(l,k)
+  	end
+end
+
+local newMain = io.open("newScript.lua", "r")
+-- write contents to new script
