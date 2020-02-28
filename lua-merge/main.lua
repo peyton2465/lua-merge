c3c3e4cf5d952ed9677c9c2fcb43f2224943d238
peyton2465 <peyton.roy@gmail.com>
2020-02-28 12:06:14 -0600

    Add Arguments

--- lua-merge/main.lua
+++ lua-merge/main.lua
@@ -1,4 +1,4 @@
-package.path = "C:\\Users\\User\\Documents\\GitHub\\lua-merge\\LuaSrcDiet\\?.lua;" .. package.path
+package.path = "LuaSrcDiet\\?.lua;" .. package.path
 local Minifier = require("init")
 
 local function findImports(script)
@@ -14,13 +14,27 @@ local function replaceImports(script, path)
 		local file = io.open(path .. i)
 		local source = replaceImports(file:read("*all"), path)
 		file:close()
+		print("Merging: " .. path .. i)
 		source = Minifier.optimize(Minifier.MAXIMUM_OPTS, source):gsub("\t+", " "):gsub("\n+", " ")
 		script = script:gsub(v:gsub("([^%w])", "%%%1"), ("(function(...)%s end)(...)"):format(source))
 	end
 	return script
 end
 
-local mainScript, newMain = io.open("test/test.lua", "r"), io.open("output.lua", "w+")
-newMain:write(replaceImports(mainScript:read("*all"), "test/"))
+local args = {...}
+local path = assert(args[1], "Invalid Path (arg 1)")
+local main = args[2] or "main.lua"
+local minify = (args[3] and args[3] == "true") or false
+local outputPath = path .. "merged.lua"
+
+print(string.format("Merging Project: %s", path .. main))
+local mainScript, newScript = io.open(path .. main, "r"), io.open(outputPath, "w+")
+local output = mainScript:read("*all")
 mainScript:close()
-newMain:close()
+if minify then
+	output = Minifier.optimize(Minifier.MAXIMUM_OPTS, output)
+end
+output = replaceImports(output, path)
+newScript:write(output)
+newScript:close()
+print("Merge Complete: " .. outputPath)
